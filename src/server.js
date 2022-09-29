36247fcaad4b0febd8e0b532267aa9bea8f6213e
vini <vribeirobarreto@gmail.com>
2022-09-29 12:50:59 -0300

    Estrutura inicial do projeto

---
+++ src/server.js
@@ -0,0 +1,25 @@
+const express = require('express')
+// const path = require('path')
+
+// módulos criados
+// const db = require('./database')
+const routes = require('./routes')
+
+
+const app = express()
+
+// conexão com o banco de dados
+// db.connect()
+
+
+// habilita server para receber dados via post (formulário)
+app.use(express.urlencoded({ extended: true }))
+
+
+// definindo as rotas
+app.use('/api', routes)
+
+
+// executando servidor 
+const port = process.env.PORT || 8080
+app.listen(port, () => console.log(`Server is listening on port ${port}`))
